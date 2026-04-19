2303c1c3f0d10f84d3202d3ef5c6bd6a3f60b345
cinemapp36 <cinemaapp36@gmail.com>
2026-04-18 20:40:09 -0500

    Deleted server.js

--- backend/src/config/mercadopago.js
+++
@@ -1,7 +0,0 @@
-const { MercadoPagoConfig } = require('mercadopago');
-
-const mpClient = new MercadoPagoConfig({
-  accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN,
-});
-
-module.exports = mpClient;
